e4b4e623a6ce4661210255b1ced464bcb93ce7d5
dpopovych72 <dpopovych@students.mchenry.edu>
2024-08-26 14:05:09 -0500

    Add files via upload

---
+++ joke.py
@@ -0,0 +1,7 @@
+# Those print functions tell a knock knock joke 
+print("\nKnock")
+print("Knock")
+print("\nWho is there? ")
+print("\nBoo")
+print("\nBoo Who ?")
+print("\nDon't cry , it's  just a joke\n")
