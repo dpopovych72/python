1c0be00f19f8ecb915e264aa676d9edd4b522ce0
dpopovych72 <dpopovych@students.mchenry.edu>
2024-09-04 13:39:06 -0500

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,11 @@
+"""Type the above code into Visual Studio Code
+       Add a minimum of 4 comments to the above Python
+       program to make it more understandable.
+    """
+
+# TODO need to write 2 more comments 
+# This program will calculate the ammount of milles that could be driven with one gallon , and will print the result 
+tank_size = 15
+distance = 600
+mpg = distance / tank_size
+print("Miles per gallon:", mpg) # This is in-line code 
