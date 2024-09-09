1072660d0ba48e7fc1bd4dc26a96a6e388f99efa
dpopovych72 <dpopovych@students.mchenry.edu>
2024-09-09 14:47:52 -0500

    Add files via upload

--- kg_to_lb_conversion.py
+++ kg_to_lb_conversion.py
@@ -1,13 +1,20 @@
 # This program will convert kilograms to pounds 
 
 # 4 killogram samples 
-kg_value_1 = 1.40
-kg_value_2 = 23.70
-kg_value_3 = 55.27
-kg_value_4 = 83.10
+kg_value_1 = 1
+kg_value_2 = 23
+kg_value_3 = 55
+kg_value_4 = 83
 
 conversion_factor = 2.20462
 lb_value_1 = kg_value_1 * conversion_factor 
 lb_value_2 = kg_value_2 * conversion_factor
 lb_value_3 = kg_value_3 * conversion_factor
 lb_value_4 = kg_value_4 * conversion_factor
+
+# Output the results 
+
+print(lb_value_1 , " pounds is equal to" , kg_value_1,"kilograms")
+print(lb_value_2 , " pounds is equal to" , kg_value_2,"kilograms")
+print(lb_value_3 , " pounds is equal to" , kg_value_3,"kilograms")
+print(lb_value_4 , " pounds is equal to" , kg_value_4,"kilograms")
