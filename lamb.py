f97aee7e9bd61c310b7409a1dfae16200bc935f0
dpopovych72 <dpopovych@students.mchenry.edu>
2024-09-09 13:37:50 -0500

    Add files via upload

---
+++ lamb.py
@@ -0,0 +1,4 @@
+name = input("Greetings , how should I call you ?")
+print("Hello , there " + name)
+age = input(" How old are you ? ")
+print("Those are the big numbers , here is your age if you were dog :" + str(int(age)*7))
