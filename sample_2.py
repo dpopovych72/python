9d1909bb659cbbd308582869bce737840b64250f
dpopovych72 <dpopovych@students.mchenry.edu>
2024-09-11 14:00:58 -0500

    Add files via upload

---
+++ sample_2.py
@@ -0,0 +1,20 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+print("Literal:", 10)
+
+# Integer
+positive_reviews = -20
+print("Positive reviews :", positive_reviews)
+
+# Float
+rating = 0.01
+print("Uber Rating :", rating)
+
+# String
+name = "Denys"
+print("Name :", name)
+
+# Boolean
+can_drive = False
+print("Knows how to drive :", can_drive)
