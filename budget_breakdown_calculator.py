a8f21639113690148aa8b76e4ca4fde988713d9e
dpopovych72 <dpopovych@students.mchenry.edu>
2024-09-14 21:32:11 -0500

    Add files via upload

--- budget_breakdown_calculator.py
+++ budget_breakdown_calculator.py
@@ -3,7 +3,7 @@
        The program will then calculate and display what percentage of 
        the total budget each category represents.
 """
-
+# Ask a user how much money he/she spent on each of the categories 
 housing = int(input("How much money did you spend on housing this month ?"))
 utilities = int(input("How much did you spend on your utilities this month ?"))
 groceries = int(input("How much did you spend on groceries this month ?"))
@@ -11,17 +11,32 @@ transportation = int(input("How much did you spend on transportation this month 
 health_care = int(input("How much have you spent on healthcare this month ?"))
 personal_care = int(input("How much did you spend on personal care this month ?"))
 clothing = int(input("How much did you spend on clothng this month ?"))
-debt = int(input("What's your debt ?"))
-budget = int(input("What's your budget this month?"))
+debt = int(input("What's your debt this month ?"))
+budget = int(input("What's your budget this month ?"))
+
+""" Divide all the results each by budget
+ and multiply by 100 to get the percentage 
+ of budget spent on each of the categories """
 
 housing_percentage = housing/budget*100
-print(housing_percentage ,"%")
+print(f"{housing_percentage:0.2f}%" )
 utilities_percentage = utilities/budget*100
-print(utilities_percentage , "%")
+print(f"{utilities_percentage:0.2f}%" )
 groceries_percentage = groceries/budget*100
-print(groceries_percentage , "%")
+print(f"{groceries_percentage:0.2f}%" )
 transportation_percentage = transportation/budget*100
+print(f"{transportation_percentage:0.2f}%" )
 health_care_percentage = health_care/budget*100
+print(f"{health_care_percentage:0.2f}%" )
 personal_care_percentage = personal_care/budget*100
+print(f"{personal_care_percentage:0.2f}%" )
 clothing_percentage = clothing/budget*100
+print(f"{clothing_percentage:0.2f}%" )
 debt_percentage = debt/budget*100
+print(f"{debt_percentage:0.2f}%" )
+
+budget_spent = housing + utilities + groceries + transportation + health_care + personal_care + clothing + debt
+print(budget_spent)
+
+budget_spent_percentage = housing_percentage + utilities_percentage + groceries_percentage + transportation_percentage + health_care_percentage + personal_care_percentage + clothing_percentage + debt_percentage
+print(budget_spent_percentage)
