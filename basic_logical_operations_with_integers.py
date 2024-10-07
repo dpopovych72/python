ecfcc40b1a6d7b1b6b69d8636e061f48ccfc1805
dpopovych72 <dpopovych@students.mchenry.edu>
2024-10-07 14:12:17 -0500

    Add files via upload

--- basic_logical_operations_with_integers.py
+++ basic_logical_operations_with_integers.py
@@ -2,8 +2,11 @@
     (and, or, not) in Python. Your task is to create a program that 
     prompts the user for two integer inputs and then demonstrates the use of these operators.
 """
+print("\n")
 
+# This function will answer all of the questions in it , using the numbers that the user provides 
 def main():
+    # Ask a user to write 2 numbers , they will be used in a function 
     number1 = int(input("Please put in a distinct integer : "))
     number2 = int(input("Please put in another distinct integer : "))
     both_greater_than_0 = False
@@ -13,32 +16,32 @@ def main():
     num1_is_equal_to_num2 = False
     num1_is_zero = False
 
-    if number1>0 and number2>0 :
+    if number1>0 and number2>0 : # Check if both numbers are greater than 0 
         both_greater_than_0 = True
         print("Both numbers are greater than 0 :",both_greater_than_0)
     else :
         print("Both numbers are greater than 0 :",both_greater_than_0)
-    if number1>100 and number2>100 :
+    if number1>100 and number2>100 : # Check if both numbers are greater than 100
         both_greater_than_100 = True
         print("Both numbers are greater than 100 :",both_greater_than_100)
     else :
         print("Both numbers are greater than 100 :",both_greater_than_100)
-    if number1 %2 == 0 or number2 %2 == 0 :
+    if number1 %2 == 0 or number2 %2 == 0 : # Check if any of the numbers are equal to zero 
         either_even = True
         print("Either number is even ? ", either_even)
     else :
         print("Either number is even ? ",either_even)
-    if number1 < 100 or number2 < 100 :
+    if number1 < 100 or number2 < 100 : # Check if any of the numbers are less than 100
         either_less_than_100 = True
         print("Either number is less than 100 ? ",either_less_than_100)
     else :
         print("Either number is less than 100 ? ",either_less_than_100)
-    if not number1 == number2 :
+    if not number1 == number2 : # Check if numbers are not same
         print("Number 1 is equal to Number 2 :", num1_is_equal_to_num2)
     else :
         num1_is_equal_to_num2 = True 
         print("Number 1 is equal to Number 2 :", num1_is_equal_to_num2)
-    if not number1 == 0 :
+    if not number1 == 0 : # Check if Number1 is not equal to zero 
         print("Number 1 is equal to 0 : ", num1_is_zero)
     else :
         num_is_zero = True
@@ -48,7 +51,7 @@ def main():
 
 
 
-main()
+main() # The written function is called in a program 
         
 
 
