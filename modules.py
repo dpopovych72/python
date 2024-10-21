d263207988559fddcffea78af7181e184f0a5600
dpopovych72 <dpopovych@students.mchenry.edu>
2024-10-21 14:47:26 -0500

    Add files via upload

--- modules.py
+++ modules.py
@@ -1,3 +1,22 @@
 """https://wiki.python.org/moin/PythonBooks
 """
+import random
 
+def main():
+
+    guess = int(input("Please guess a number from 1 to 100 : "))
+    random_value = random.randint(1,100)
+    difference = abs(random_value)
+
+    while not guess == random_value :
+        guess_again = int(input("Please guess again : "))
+        guess = guess_again
+        print(difference)
+        print(f"The random value is {random_value}")
+    else : 
+        print("Your answer is correct")
+
+    
+
+
+main()
