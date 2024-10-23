d43bc3b14bc4d5f485dbfb20f8ff43e3f9286a1e
dpopovych72 <dpopovych@students.mchenry.edu>
2024-10-23 14:47:39 -0500

    Add files via upload

--- ticket_sale.py
+++ ticket_sale.py
@@ -4,20 +4,32 @@
 """
 
 free_seats = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-print("Those seats are available :" , free_seats )
+taken_seat = []
+ticket_list = []
 
-buy_seat = int(input("Please enter a seat that you would like to take " ))
 
 def main():
+    print("Those seats are available :" , free_seats)
+    buy_seat = int(input("Please enter a seat that you would like to take " ))
+    
+
+
+# This code is detailed but very messy , and so unfinished  
+"""def main():
     if buy_seat in free_seats :
         print(f"Seat {buy_seat} is available")
         take_it = input(f"Do you want to take it ? yes/no ")
-        if take_it in ["yes","y"] :
+        while take_it in ["yes","y"] :
             print(f"You took seat : {buy_seat}")
             free_seats.remove(buy_seat)
+            taken_seat.append(buy_seat)
             print(f"Free seats left : ", free_seats)
         if take_it in["no","n"] :
             plan_to_pick = input("Do you plan to pick any seat ? (yes/no)" )
+            if plan_to_pick in ["no","n"] :
+                for i in  range(len(taken_seat)):
+                    print("You took those seats : " , taken_seat)
+                print("Those seats are still available " , free_seats)
             while plan_to_pick in ["yes","y"]:
                 pick_another_seat = int(input("Pick another seat"))
                 print("You picked seat:" , pick_another_seat)
@@ -25,7 +37,7 @@ def main():
             else :
                 print("You are free to leave " )
     else :
-        print("Seat is not available ")
+        print("Seat is not available ")"""
 
 
-main()
+
