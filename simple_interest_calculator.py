a49fb6b0daedea7896f284b125f17dbebe87ba5e
dpopovych72 <dpopovych@students.mchenry.edu>
2024-10-30 11:44:00 -0500

    Update simple_interest_calculator.py

--- simple_interest_calculator.py
+++ simple_interest_calculator.py
@@ -5,12 +5,15 @@
 # Simple Interest = (Principal Amount × Rate of Interest × Time) / 100
 def calculate_interest(principal , rate , time):
     simple_interest = (principal*rate*time)/100
-    print(simple_interest)
+    return simple_interest
 
 # Ask user for numbers 
-principal_ammount= float(input("Write a principal ammount"))
-rate_of_interest = float(input("Write an interest rate in percents"))
-time_ammount = int(input("Write time in years"))
+principal_ammount= float(input("Write a principal ammount ($): "))
+rate_of_interest = float(input("Write an interest rate (%) : "))
+time_ammount = int(input("Write time (years) : "))
 
 # Use the provided values for parameters , and call the function 
-calculate_interest(principal=principal_ammount,rate=rate_of_interest,time=time_ammount)
+result = (calculate_interest(principal_ammount,rate_of_interest,time_ammount))
+print(f"The simple interest for a principal ammount of ${principal_ammount:0.02f}\n \
+at a rate of interest {rate_of_interest:0.02f}% \
+over a period of {time_ammount} years is $",result)
