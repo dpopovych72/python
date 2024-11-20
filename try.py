7b9a8c4d83bef12851f375b08ba2b06eb4a7734d
dpopovych72 <dpopovych@students.mchenry.edu>
2024-11-20 14:49:25 -0600

    modules folder , entire

--- try.py
+++ try.py
@@ -1,5 +1,12 @@
-list_of_items = [ 1,2,3,4,5]
+# list_of_items = [ 1,2,3,4,5]
 
-for i in list_of_items:
-    print(i)
+# for i in list_of_items:
+#     print(i)
+guess = 9 
+number = 4
+print(abs(number))
+print(abs(guess))
+difference = abs(number)-abs(guess)
+difference = abs(difference)
+print(difference)
 
